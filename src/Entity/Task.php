ed7c20d7249f573ef807501afc10aff75bb48bc4
Jaylaw <jelorence2004@gmail.com>
2026-02-06 16:17:46 +0800

    Your Message

---
+++ src/Entity/Task.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource(operations: [
+    new GetCollection(),
+    new Post(),
+    new Get(),
+    new Put(),
+    new Patch(),
+    new Delete(),
+])]
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+    public function __construct()
+    {
+        $this->isCompleted = false;
+        $this->createdAt = new \DateTimeImmutable();
+}
+
+}
